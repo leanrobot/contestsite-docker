5039be28bb62eff62fd502b301f84d0fe5aec7a2
Tom Petit <thopet@uw.edu>
2015-06-22 14:19:55 -0700

    standardized docker entrypoint

--- run_dev.sh
+++ run_dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run rabbitmq
-docker rm -fq contestsite rabbitmq celeryworker
+docker rm -f contestsite rabbitmq celeryworker
 rabbitmq_id=$(docker run -d --name rabbitmq rabbitmq:latest)
-worker_id=$(docker run -d  --name celeryworker leanrobot/contestsite
+worker_id=$(docker run -d --name celeryworker leanrobot/contestsite
 	python manage.py celery worker)
 docker run --rm -t -i -p 8080:80 --link rabbitmq:broker --name contestsite -v \
 	~/programming/ContestSite/django:/root/contestsite \
