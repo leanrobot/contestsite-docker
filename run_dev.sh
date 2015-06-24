16a0d698b37dc648e941679c823164ebec3d14c1
Tom Petit <thopet@uw.edu>
2015-06-24 02:57:28 -0700

    docker-compose: added development yaml file

--- run_dev.sh
+++ run_dev.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # run rabbitmq
-docker rm -f contestsite rabbitmq celeryworker
-rabbitmq_id=$(docker run -d --name rabbitmq rabbitmq:latest)
-worker_id=$(docker run -d --name celeryworker leanrobot/contestsite
-	python manage.py celery worker)
-docker run --rm -t -i -p 8080:80 --link rabbitmq:broker --name contestsite -v \
-	~/programming/ContestSite/django:/root/contestsite \
-	leanrobot/contestsite
-
+docker-compose -f docker-compose-dev.yml up
