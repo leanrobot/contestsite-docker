bd5b44582cf6d2724ab6914f4b995b813d824d9f
Tom Petit <thopet@uw.edu>
2015-07-02 15:11:00 -0700

    bash access improved: upgraded to docker-compose

--- bash.sh
+++ bash.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-docker rm -f contestsite rabbitmq celeryworker
-docker run -d --name rabbitmq rabbitmq:latest 
-docker run -d -e C_FORCE_ROOT=true --name celeryworker -v ~/programming/ContestSite/django:/root/contestsite --link rabbitmq:broker leanrobot/contestsite python manage.py celery worker
-docker run --rm -t -i -p 8080:80 --link rabbitmq:broker --link celeryworker:worker \
-	--name contestsite \
-	-v ~/programming/ContestSite/django:/root/contestsite \
-	-v ~/programming/ContestSite/db.sqlite:/root/contestsite/db.sqlite \
-	leanrobot/contestsite /bin/bash
+docker-compose -f docker-compose-dev.yml run --service-ports webserver /bin/bash
+docker-compose -f docker-compose-dev.yml kill
+docker-compose -f docker-compose-dev.yml rm -f
